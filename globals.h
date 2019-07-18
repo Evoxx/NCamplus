8a0189dfde1c7b977a213236cd1fb00fa5d6a29e
evoxx <evolutyons@yahoo.es>
2019-07-18 13:11:37 +0200

    Bump to version 10.3

--- globals.h
+++ globals.h
@@ -366,8 +366,8 @@ typedef uint8_t uint8_t;
 /* ===========================
  *         constants
  * =========================== */
-#define CS_VERSION    "10.1"
-#define DATE_BUILD    "13-06-2019"
+#define CS_VERSION    "10.3"
+#define DATE_BUILD    "18-07-2019"
 #define CS_REVISION   "r1"
 #ifndef CS_SVN_VERSION
 #define CS_SVN_VERSION "stable"
